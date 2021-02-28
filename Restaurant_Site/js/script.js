ed3a43337cb422c5fc6a6e71f5e7a1ec2020cca2
theAdarshSrivastava <77750467+theAdarshSrivastava@users.noreply.github.com>
2021-02-28 16:39:13 +0530

    Update script.js

--- Restaurant_Site/js/script.js
+++ Restaurant_Site/js/script.js
@@ -32,7 +32,7 @@ var insertHtml = function (selector, html) {
 // Show loading icon inside element identified by 'selector'.
 var showLoading = function (selector) {
   var html = "<div class='text-center'>";
-  html += "<img src='images/ajax-loader.gif'></div>";
+  html += "<img src='img/ajax-loader.gif'></div>";
   insertHtml(selector, html);
 };
 
