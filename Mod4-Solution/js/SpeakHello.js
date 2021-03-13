bd2121fc2f33bab4058df10b6c84ba756b68d46b
Adarsh Srivastava <77750467+theAdarshSrivastava@users.noreply.github.com>
2021-03-13 11:23:27 +0530

    Delete SpeakHello.js

--- Mod4-Solution/js/SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function (window) {  
-    var helloSpeaker = {};
-    var speakWord = "Hello";
-    
-    helloSpeaker.speak = function (names) {
-      console.log(speakWord + " " + names);
-    }
-        window.helloSpeaker = helloSpeaker;
-    })(window);
